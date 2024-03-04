b803563447ce8b2955e969b2c22bbaccff66d629
Kunter <kunter.grupmann@voco.ee>
2024-03-04 09:54:16 +0200

    Displayed an article by slug using the article oop model

--- routes/articles.js
+++ routes/articles.js
@@ -7,4 +7,6 @@ const articleController = new articleControllerClass()
 
 router.get('/', (req, res) => articleController.getAllArticles(req, res));
 
+router.get('/article/:slug', (req, res) => articleController.getArticleBySlug(req, res));
+
 module.exports = router;
